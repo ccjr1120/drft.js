19ace1445f17696b200212a1aaeceab5692fc0bb
ccjr <qianaiwuyu@gmail.com>
2023-10-16 00:09:12 +0800

    ✨ feat: 1

--- src/renderer/CanvasKit/index.ts
+++ src/renderer/CanvasKit/index.ts
@@ -1,7 +1,18 @@
+import CanvasKitInit, {
+  CanvasKit,
+} from '../../../node_modules/canvaskit-wasm/types/index'
 import DrftRenderer from '../DrftRenderer'
 
 class CanvasKitRenderer extends DrftRenderer {
-  
+  CanvasKit: CanvasKit
+
+  constructor() {
+    super()
+    CanvasKitInit().then((CanvasKit) => {
+      console.log(this.CanvasKit)
+      this.CanvasKit = CanvasKit
+    })
+  }
 }
 
 export default CanvasKitRenderer
